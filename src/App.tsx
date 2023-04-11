72a0c8dc4e338165c3d184fbb47a96052307f76d
Mario Arro <marioarro186@gmail.com>
2023-04-11 21:38:55 +0300

    updated styling to the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,25 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { Server } from './Server';
-import { DynamicList } from './DynamicList';
 import { RDM_Device } from './RDM_Device';
 import DeviceList from './DeviceList';
-
-let g_Server: Server;
-let g_DeviceList: DynamicList;
 
 const App = () => {
   const [deviceList, setDeviceList] = useState([]);
   const [modifiedList, setModifiedList] = useState([]);
-  // window.onload = () => {
-  //   main();
-  // };
-
-  function main() {
-    g_Server = new Server({
-      device_added_callback: (device_data: RDM_Device) => {
-        // Called when a new RDM Device has been discovered.
-        // Create an RDM Device entry in the RDM Device List with the values in device_data.
-
-        setDeviceList((prevDevices) => [...prevDevices, device_data]);
-        setModifiedList((prevList) => [...prevList, device_data]);
-      },
-      // device_updated_callback: (device_data: RDM_Device) => {
-      //   // Called when an RDM Device parameter change is detected.
-      //   // Update existing associated RDM Device entry in the RDM Device List with the values in device_data.
-      //   console.log('Update Device', device_data);
-      //   console.log(device_data);
-      //   // setDeviceList((prevDevices) => [...prevDevices, device_data]);
-      // },
-      device_updated_callback: (device_data: RDM_Device) => {
-        setDeviceList((prevDevices) => {
-          // Find the device in the previous list of devices
-          const index = prevDevices.findIndex((device) => device.uid === device_data.uid);
-          // If the device is not found, just return the previous list of devices
-          if (index === -1) {
-            return prevDevices;
-          } else {
-            // If the device is found, update its data and return the new list of devices
-            const newDevices = [...prevDevices];
-            newDevices[index] = device_data;
-            return newDevices;
-          }
-        });
-      },
-    });
-    // Use Server.GetDeviceCount() to get number of devices in backend device list
-    console.log('Current Device Count: ', g_Server.GetDeviceCount());
-    // // Use Server.GetDeviceByIndex() to get backend device by index (index 0 - first added device, index 2 - third added device, ...)
-    // console.log('First Device: ', g_Server.GetDeviceByIndex(0));
-
-    // document.getElementById('filter_none').onclick = () => {
-    //   console.log('Set DynamicList filter to show all devices');
-    // };
-
-    // document.getElementById('filter_na').onclick = () => {
-    //   console.log('Set DynamicList filter to show devices if RDM_Device.manufacturer == "Company NA"');
-    // };
-
-    // document.getElementById('filter_tmb').onclick = () => {
-    //   console.log('Set DynamicList filter to show devices if RDM_Device.manufacturer == "TMB"');
-    // };
-
-    // document.getElementById('sort_uid').onclick = () => {
-    //   console.log('Set DynamicList sort mode to RDM_Device.uid_value');
-    // };
-
-    // document.getElementById('sort_address').onclick = () => {
-    //   console.log('Set DynamicList sort mode to RDM_Device.address');
-    // };
-
-    // document.getElementById('sort_manufacturer').onclick = () => {
-    //   console.log('Set DynamicList sort mode to RDM_Device.manufacturer');
-    // };
-  }
+  const [activeFilter, setActiveFilter] = useState('None');
+  const [activeSorting, setActiveSorting] = useState(null);
 
   //Sorting by device user ID in ascending order
   const sortByUID = () => {
-    const sortedByUID = deviceList.sort((a, b) => (a.uid < b.uid ? -1 : a.uid > b.uid ? 1 : 0));
+    const sortedByUID = modifiedList.sort((a, b) => (a.uid < b.uid ? -1 : a.uid > b.uid ? 1 : 0));
+    setActiveSorting('By UID');
     setModifiedList([...sortedByUID]);
   };
 
   //Sorting by device address in ascending order
   const sortByAddress = () => {
-    const sortedByAddress = deviceList.sort((a, b) => {
+    const sortedByAddress = modifiedList.sort((a, b) => {
       //If the device addresses are not equal, then sort them by address
       if (a.address !== b.address) {
         return a.address - b.address;
@@ -94,12 +28,13 @@ const App = () => {
         return a.uid < b.uid ? -1 : a.uid > b.uid ? 1 : 0;
       }
     });
+    setActiveSorting('By Address');
     setModifiedList([...sortedByAddress]);
   };
 
   //Sorting by device manufacturer in alphabetical order
   const sortByManufacturer = () => {
-    const sortedByManufacturer = deviceList.sort((a, b) => {
+    const sortedByManufacturer = modifiedList.sort((a, b) => {
       //If the device manufacturers are not equal then sort them by manufacturer name
       if (a.manufacturer !== b.manufacturer) {
         const manufacturerA = a.manufacturer.toUpperCase(); // ignore upper and lowercase
@@ -116,66 +51,58 @@ const App = () => {
         return a.uid < b.uid ? -1 : a.uid > b.uid ? 1 : 0;
       }
     });
-
+    setActiveSorting('By Manufacturer');
     setModifiedList([...sortedByManufacturer]);
   };
 
   const filterNA = () => {
     const filterLogic = deviceList.filter((device) => device.manufacturer === 'Company NA');
+    setActiveFilter('Company NA');
     setModifiedList([...filterLogic]);
   };
 
   const filterTMB = () => {
     const filterLogic = deviceList.filter((device) => device.manufacturer === 'TMB');
+    setActiveFilter('TMB');
     setModifiedList([...filterLogic]);
   };
 
   const clearFilters = () => {
+    setActiveFilter('None');
     setModifiedList(deviceList);
   };
 
-  // const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-  // const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-  // if (nameA < nameB) {
-  //   return -1;
-  // }
-  // if (nameA > nameB) {
-  //   return 1;
-  // }
-
-  // // names must be equal
-  // return 0;
-
   useEffect(() => {
-    // const server = new Server({
-    //   device_added_callback: (device_data: RDM_Device) => {
-    //     setDeviceList((prevDevices) => [...prevDevices, device_data]);
-    //   },
-    //   device_updated_callback: (device_data: RDM_Device) => {
-    //     setDeviceList((prevDevices) => {
-    //       const index = prevDevices.findIndex((device) => device.uid === device_data.uid);
-    //       if (index === -1) {
-    //         return prevDevices;
-    //       }
-    //       const newDevices = [...prevDevices];
-    //       newDevices[index] = device_data;
-    //       return newDevices;
-    //     });
-    //   },
-    // });
+    const server = new Server({
+      device_added_callback: (device_data: RDM_Device) => {
+        // Called when a new RDM Device has been discovered.
+        // Create an RDM Device entry in the RDM Device List with the values in device_data.
 
-    main();
+        setDeviceList((prevDevices) => [...prevDevices, device_data]);
+        setModifiedList((prevList) => [...prevList, device_data]);
+      },
+
+      device_updated_callback: (device_data: RDM_Device) => {
+        // Called when an RDM Device parameter change is detected.
+        // Update existing associated RDM Device entry in the RDM Device List with the values in device_data.
+
+        setDeviceList((prevDevices) => {
+          // Find the device in the previous list of devices
+          const index = prevDevices.findIndex((device) => device.uid === device_data.uid);
+          // If the device is not found, just return the previous list of devices
+          if (index === -1) {
+            return prevDevices;
+          } else {
+            // If the device is found, update its data and return the new list of devices
+            const newDevices = [...prevDevices];
+            newDevices[index] = device_data;
+            return newDevices;
+          }
+        });
+      },
+    });
   }, []);
 
-  // console.log(g_DeviceList);
-  // console.log(Server);
-  // const currentDeviceList = () => {
-  //   console.log(g_Server);
-  //   const devList = g_Server.GetDeviceList();
-  //   setDeviceList(devList);
-  // };
-
-  // console.log(deviceList);
   return (
     <div>
       <div id="test_frame" className="frame">
@@ -243,9 +170,11 @@ const App = () => {
         </div>
       </div>
       <div id="list_frame" className="frame">
-        {/* <span>
-          RDM Device List (${FILTER_VISIBLE_COUNT}/${DeviceList.length} | ${FILTER_MODE} | ${SORT_MODE})
-        </span> */}
+        <span>
+          RDM Device List: {deviceList.length} | Filtered Device List: {modifiedList.length} | Active Filter:{' '}
+          {activeFilter} | Active Sorting Method:
+          {activeSorting}
+        </span>
         <div id="rdm_device_list">
           <table className="na-table" style={{ width: '100%' }}>
             <tr className="rdm-list-header na-table-header">
--- src/DeviceList.tsx
+++ src/DeviceList.tsx
@@ -8,16 +8,23 @@ const DeviceList = ({ modifiedList }: any) => {
         <tr key={device.uid} className="table-data">
           <td className={device.is_online ? 'na-table-row-green' : 'na-table-row-red'}>{}</td>
           <td>{device.uid.slice(0, 4) + ' : ' + device.uid.slice(4, device.uid.length)}</td>
-          <td className="table-column-borders">
-            {'Test' + device.label.slice(0, 6) + ' #' + device.label.slice(7, device.label.length)}
+          <td>
+            <input
+              type="text"
+              value={'Test' + device.label.slice(0, 6) + ' #' + device.label.slice(7, device.label.length)}
+              className="table-label-input"
+            />
           </td>
           <td>{device.manufacturer}</td>
           <td>{device.model}</td>
           <td style={{ color: 'white' }} className="text-white">
-            Mode #{device.mode_index}
+            <select>
+              <option>Mode #{device.mode_index}</option>
+            </select>
           </td>
-          <td style={{ color: 'white' }} className="text-white">
-            {device.address}
+          <td className="text-white">
+            <input type="text" value={device.address} className="table-address-input" />
+            {/* {device.address} */}
           </td>
         </tr>
       ))}
